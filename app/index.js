7273055e7dbc6b99eec3b9ae7c72e7c1ee3dc7e6
Jonathan Devers <jonathan.devers@gmail.com>
2016-03-05 15:57:48 -0500

    complete first test component

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-DOM');
 
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <div> Hello Fatty! </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+);
